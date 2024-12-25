e7c83ee2eaa56581684e58c62e0f22a7015eb691
Faris Sharara <faresshohdy0@gmail.com>
2024-12-25 23:38:42 +0200

    feat: add Analytics page with pie chart and performance summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import About from './compnents/About'
+import About from './Pages/About'
+import Analytics from './Pages/Analytics';
 
 function App() {
   return(
   <Router>
   <Routes>
-    <Route path="/" element={<About />} />
+    <Route path="/" element={<Analytics />} />
   </Routes>
 </Router>
   )
