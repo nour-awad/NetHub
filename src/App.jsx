e5f33340bfc0b3e638ff88ae8fb9b4aa25090a1d
nour-awad <nour.awad094@gmail.com>
2024-12-27 15:11:26 +0200

    fetching the rivia questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Analytics from "./Pages/Analytics";
-import AnswerFeedback from "./Pages/Answer";
 
 function App() {
   return (
-
+    <div>
+      <h1>Triviaa</h1>
       <Router>
-        <Routes>
-          <Route path="/anlytics" element={<Analytics />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Router>
-    
+      <Routes>
+        
+        <Route path="/" element={<Analytics />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
     </div>
   );
-
-    <div>
-      <TrivolioFooter/>
-    </div>
 }
 
 export default App;
