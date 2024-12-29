efcf89fd890c87b7f0cfd7e2de6be17ed77407ff
nour-awad <nour.awad094@gmail.com>
2024-12-29 12:35:29 +0200

    merging the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Analytics from "./Pages/Analytics";
-import AnswerFeedback from "./Pages/Answer";
+import About from "./Pages/About";
+import Trivia from "./Pages/TriviaQuestions";
 
 function App() {
   return (
     <div>
-      <h1>Triviaa</h1>
       <Router>
         <Routes>
-          <Route path="/anlytics" element={<Analytics />} />
+          <Route path="/" element={<Trivia />} />
+          <Route path="/analytics" element={<Analytics />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
-    
     </div>
   );
 
