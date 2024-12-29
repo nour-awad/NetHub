60232ee54f3ddd57f13300194921d14c6693cee5
nour-awad <nour.awad094@gmail.com>
2024-12-29 13:50:55 +0200

    Revert "Completed merge and resolved conflicts"
    This reverts commit 7fe8a48369059aa653c2d63337092262841d8a19, reversing
    changes made to d01aa8c120e5ae549c38607f7261e5ee915ccdfa.

--- src/Pages/Analytics.jsx
+++ src/Pages/Analytics.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
-import "../Pages/css/Analytics.css";
 
 const Analytics = ({ correct, wrong }) => {
   const data = [
@@ -9,36 +8,30 @@ const Analytics = ({ correct, wrong }) => {
   ];
 
   return (
-    <div className="analytics-container container text-center mt-5">
-      <h1 className="analytics-title mb-4">Analytics</h1>
-      <h2 className="analytics-subtitle mb-4">Trivia Performance</h2>
-      <div className="row">
-        <div className="col-md-6">
-          <PieChart width={400} height={400} className="pie-chart">
-            <Pie
-              data={data}
-              dataKey="value"
-              outerRadius={150}
-              label={({ name, percent }) =>
-                `${name}: ${(percent * 100).toFixed(0)}%`
-              }
-            >
-              <Cell fill="#0088FE" />
-              <Cell fill="#FF8042" />
-            </Pie>
-            <Tooltip />
-            <Legend />
-          </PieChart>
-        </div>
-        <div className="col-md-6 d-flex flex-column justify-content-center align-items-start">
-          <p className="analytics-text">Total Questions: {correct + wrong}</p>
-          <p className="analytics-text">Correct Answers: {correct}</p>
-          <p className="analytics-text">Incorrect Answers: {wrong}</p>
-          <p className="analytics-text">
-            Correct Percentage:{" "}
-            {((correct / (correct + wrong || 1)) * 100).toFixed(2)}%
-          </p>
-        </div>
+    <div>
+      <h1>Analytics</h1>
+      <h2>Trivia Performance</h2>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          dataKey="value"
+          outerRadius={150}
+          label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
+        >
+          <Cell fill="#0088FE" />
+          <Cell fill="#FF8042" />
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+      <div>
+        <p>Total Questions: {correct + wrong}</p>
+        <p>Correct Answers: {correct}</p>
+        <p>Incorrect Answers: {wrong}</p>
+        <p>
+          Correct Percentage:{" "}
+          {((correct / (correct + wrong || 1)) * 100).toFixed(2)}%
+        </p>
       </div>
     </div>
   );
