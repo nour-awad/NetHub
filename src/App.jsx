da0b1ccf22544710e79ddfe6fa0cf3bbb1ac77d0
nour-awad <nour.awad094@gmail.com>
2024-12-29 12:38:06 +0200

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Analytics from "./Pages/Analytics";
-<<<<<<< HEAD
 import About from "./Pages/About";
 import Trivia from "./Pages/TriviaQuestions";
-=======
 import AnswerFeedback from "./Pages/Answer";
 import About from "./Pages/About";
->>>>>>> refs/remotes/origin/main
+
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <Router>
         <Routes>
           <Route path="/" element={<Trivia />} />
@@ -21,7 +18,6 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
-=======
       <h1>Navbar Simulation</h1>
       <Router>
         <Routes>
@@ -31,7 +27,6 @@ function App() {
       </Router>
       <h1>Footer Simulation</h1>
     
->>>>>>> refs/remotes/origin/main
     </div>
   );
 }
