1fe5461a91a2ead5b49d7a9330d1bb67fe819d57
Faris Sharara <faresshohdy0@gmail.com>
2024-12-29 12:19:39 +0200

    feat: update About page layout and styling; add Navbar and Footer simulations

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Analytics from "./Pages/Analytics";
 import AnswerFeedback from "./Pages/Answer";
+import About from "./Pages/About";
 
 function App() {
   return (
     <div>
-      <h1>Triviaa</h1>
+      <h1>Navbar Simulation</h1>
       <Router>
         <Routes>
+        <Route path="/about" element={<About />} />
           <Route path="/anlytics" element={<Analytics />} />
-          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
+      <h1>Footer Simulation</h1>
     
     </div>
   );
-
-    <div>
-      <TrivolioFooter/>
-    </div>
 }
 
 export default App;
