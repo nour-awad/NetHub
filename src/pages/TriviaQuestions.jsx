7a548bf395576da9a524aa1fa620190a5c9eef58
nour-awad <nour.awad094@gmail.com>
2024-12-29 19:36:13 +0200

    enhanced the trivia questions css

--- src/pages/TriviaQuestions.jsx
+++ src/pages/TriviaQuestions.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AnswerFeedback from '../Pages/Answer.jsx';
+import Navbar from '../Pages/Navbar.jsx';
 import '../Pages/css/Trivia.css';
 
 function Trivia({ onUpdateAnalytics }) {
@@ -73,6 +74,7 @@ function Trivia({ onUpdateAnalytics }) {
 
   return (
     <div className="container mt-5">
+      <Navbar/>
       <h1>Trivia Questions</h1>
       {questionData && (
         <div className="question-container">
